1059a51e5dc92580586e85eae9f1a9c352c29119
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-08-17 19:46:45 +0300

    commit tree 7c4be2c4451ba096e8bca84bb62f9a1c6154c38b

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
-
+def name=(person_names)
+    @name=person_names
 end
+def job=(person_jobs)
+    @job=person_jobs
+end
+def name
+    @name
+  end
+  def job
+    @job
+  end
+end
+denno=Person.new
+denno.name="Denno"
+denno.job="Software Developer"
+
+puts denno.name
+puts denno.job
